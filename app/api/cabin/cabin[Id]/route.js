c9c5f3a2c722e57bb6bcb87b2a9dc19748b146ad
Matos890 <matosyaghmai@gmail.com>
2025-03-17 14:21:48 +0100

    'thirdVercel'

--- app/api/cabin/cabin[Id]/route.js
+++ app/api/cabin/cabin[Id]/route.js
@@ -2,14 +2,17 @@ import { getBookedDatesByCabinId, getCabin } from "@/app/_lib/data-service";
 
 export async function GET(request, { params }) {
   const { cabinId } = params;
+
   try {
     const [cabin, bookedDates] = await Promise.all([
       getCabin(cabinId),
       getBookedDatesByCabinId(cabinId),
     ]);
+
     return Response.json({ cabin, bookedDates });
-  } catch (error) {
-    console.error("Error fetching cabin data:", error); //Aggiunto log di errore
-    return Response.json({ message: "cabin not found" }, { status: 404 }); //Aggiunto status code
+  } catch {
+    return Response.json({ message: "Cabin not found" });
   }
 }
+
+// export async function POST() {}
